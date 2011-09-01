1ef850873905530b695639f471f79ef3f72e0aa4
Dmitry <masterrr1995@gmail.com>
2011-09-01 21:44:41 +0300

    fixed encoding

--- array_mod_1_2_5.pas
+++ array_mod_1_2_5.pas
@@ -1,4 +1,4 @@
-// заполнен массив n-числами, сколько чисел делится на 7 с остатком 1,2,5?
+// array of n-numbers, how many numbers the remainder of the division is 1, 2 or 5
 program program1;
 var
    numbers: array [0..1000] of integer;
@@ -9,7 +9,7 @@ begin
      s1 := 0;
      s2 := 0;
      s5 := 0;
-for i:=0 to 333 do  //заполняем массив числами от 0 до 20
+for i:=0 to 333 do  //fill the array with numbers from 0 to 20
     begin
          numbers[i] := i;
          inc(i);
@@ -23,8 +23,8 @@ for i:=0 to 333 do
          if numbers[i] mod 7 = 5 then
             inc(s5);
 end;
-    writeln('Числа, делющееся на 7 с остатком 1 --- ', s1);
-    writeln('Числа, делющееся на 7 с остатком 2 --- ', s2);
-    writeln('Числа, делющееся на 7 с остатком 5 --- ', s5);
-    writeln('Количетсво чисел,которые делятся на 1, 2 и 5 с остатком ', s1 + s2 + s5);
+    writeln('i mod 7 = 1 --- ', s1);
+    writeln('i mod 7 = 2 --- ', s2);
+    writeln('i mod 7 = 5 --- ', s5);
+    writeln('Summ: ', s1 + s2 + s5);
 end.
