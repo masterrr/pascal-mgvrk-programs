2bee2adc4277c44a698503d1d148d7bbd0517603
Dmitry <masterrr1995@gmail.com>
2011-09-01 21:46:10 +0300

    fixed emptyline

--- array_mod_1_2_5.pas
+++ array_mod_1_2_5.pas
@@ -2,19 +2,19 @@
 program program1;
 var
    numbers: array [0..1000] of integer;
-   i: integer;
-   s1,s2,s5: integer;
-   x: real;
+   i : integer;
+   s1,s2,s5 : integer;
+   x : real;
 begin
      s1 := 0;
      s2 := 0;
      s5 := 0;
-for i:=0 to 333 do  //fill the array with numbers from 0 to 20
+for i := 0 to 333 do  //fill the array with numbers from 0 to 20
     begin
          numbers[i] := i;
          inc(i);
     end;
-for i:=0 to 333 do
+for i := 0 to 333 do
     begin
          if numbers[i] mod 7 = 1 then
             inc(s1);
