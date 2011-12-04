b4a6a33cd0f4bfd4b41963098001b315f98e061a
Dmitry Master <masterrr1995@gmail.com>
2011-12-04 13:15:22 +0300

    completed

--- L7/L7_3.pas
+++ L7/L7_3.pas
@@ -2,7 +2,7 @@ program L7_3;
 var
 f1, f2: text;
 m: array [1..100, 1..100] of integer;
-size, i, j: integer;
+size, i, j, sum, i2, c: integer;
 begin
 assign(f1, 'in.txt');
 assign(f2, 'out.txt');
@@ -12,13 +12,21 @@ reset(f1);
          readln(f1);
          inc(size);
     end;
-                    for i:=1 to size do
-                    begin
-                        for j:=1 to size do
-                            read(f1, m[i,j]);
-                    readln(f1);
-                    end;
-
-writeln(' ', m[1,1]);
 close(f1);
+reset(f1);
+    for i:=1 to size do
+    begin
+        for j:=1 to size do
+            read(f1, m[i,j]);
+    readln(f1);
+    end;
+close(f1);
+c:=size;
+for i:=1 to size do
+begin
+     for i2:=1 to c do
+         sum:=sum+m[i,i2];
+    dec(c);
+end;
+writeln(sum);
 end.
